5722ec8ebc42659efa0efc05ccfc9259a5c9ed1f
Pavel Salajka <salajkapavel@gmail.com>
2025-09-21 19:47:24 +0200

    Initial commit

---
+++ cv1/cv1.py
@@ -0,0 +1,5 @@
+# Implementace objektivních funkcí a blind search
+# Optimalizace funkcí pro hledání globálního minima
+# Implementovat 9 funkcí a prohnat je blind searchem s vizualizací, bonus bod za animaci
+# Hill climbing algoritmus a prohnat jím funkce
+# Blind search hází náhodné body, zatímco hill climbing prozkoumává okolí
